c2566c2694d4efe0c12f1a4eea91913a9285230e
Ismael Saavedra <dev@ismaelsaavedra.name>
2023-03-23 08:47:02 +0100

    + V1 + Github action release

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ScText>V1</ScText>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const ScText = styled(Text)`
+  font-size: 24px;
+`
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 24
   },
 });
--- readme.md
+++ readme.md
@@ -7,5 +7,14 @@ https://docs.expo.dev/build/setup/
 # Upgrading to Yarn v3
 https://yarnpkg.com/getting-started/migration
 
-# Gitignore for v3
+# Gitignore for Yarn V3
 https://yarnpkg.com/getting-started/qa#which-files-should-be-gitignored
+
+# Github Actions - Setup Expo Updates
+https://github.com/expo/expo-github-action
+
+# Create Expo Token for GH Action
+https://expo.dev/accounts/%5Baccount%5D/settings/access-tokens
+
+# Triggers - On Release
+https://docs.github.com/en/webhooks-and-events/webhooks/webhook-events-and-payloads#release
